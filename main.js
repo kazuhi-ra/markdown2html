5b5974127fcc021cdb8a4ac6213cd05a74c14158
kazuhi_ra <kazuhira4126@gmail.com>
2020-01-22 17:22:56 +0900

    marked部分のユニットテスト

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 const program = require('commander');
 const fs = require('fs');
-const marked = require('marked');
+const md2html = require('./md2html');
 
 program.option('--gfm', 'GFMを有効にする');
 program.parse(process.argv);
 const filePath = program.args[0];
 
-const cliOptons = {
+const cliOptions = {
   gfm: false,
   ...program.opts(),
 };
+
+console.log(cliOptions);
 
 fs.readFile(filePath, { encoding: 'utf8' }, (err, file) => {
   if (err) {
     console.error(err.message);
     process.exit(1);
-  } else {
-    const html = marked(file, { gfm: cliOptons.gfm });
-    console.log(html);
+    return;
   }
+  const html = md2html(file, cliOptions)
+  console.log(html);
 });
+
